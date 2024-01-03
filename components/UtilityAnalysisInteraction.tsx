988dc16bb609513338be24ef30fe3d550be6a9d8
RobinMuehBFH <robin.muehlemann@students.bfh.ch>
2024-01-03 14:25:30 +0100

    removed dark mode and changed max length of description

--- components/UtilityAnalysisInteraction.tsx
+++ components/UtilityAnalysisInteraction.tsx
@@ -258,7 +258,7 @@ const UtilityAnalysisInteraction = ({ crowdfundingProviderData }: { crowdfunding
                                         {selectedProvider && selectedProvider.name}
                                       </h2>
                                       <p className="mt-2 text-sm text-gray-800" title="New York, NY 10004, United States">
-                                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor t.
                                       </p>
                                     </div>
 
