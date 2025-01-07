3f750043828d89a1fc2248ec21abf8aa81a7f314
TamaStefBfh <https://github.com/TamaStefBfh/gymi_course_crawler.git>
2025-01-07 23:27:51 +0100

    Anon und URL Consol.log entfernt

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -14,5 +14,4 @@ if (!supabaseUrl || !supabaseAnonKey) {
 export const createServerSupabaseClient = () => {
   return createClient<Database>(supabaseUrl, supabaseAnonKey);
 };
-console.log('Supabase URL:', process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log('Supabase Anon Key:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
